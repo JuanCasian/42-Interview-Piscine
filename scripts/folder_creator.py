d4a706770e9a3a37a193c1cbec48c25e1a751b7e
Juan Casian <juanpedrocasian@gmail.com>
2019-03-25 10:04:17 -0700

    adding folder creator script

---
+++ scripts/folder_creator.py
@@ -0,0 +1,11 @@
+import os, sys
+
+args = sys.argv
+if (len(args) < 3):
+    print("Specify folder and number of folders to be created")
+    exit()
+max_folder = int(args[2])
+for i in range(max_folder + 1):
+    os.system("mkdir {}/ex{:02d}".format(args[1], i))
+    print("creating folder: {}/ex{:02d}".format(args[1], i))
+print("ready")
